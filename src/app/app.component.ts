ae498bbcfa9a9bcd59e0499a34ca0abd533ce911
dpearyer <dpear1050@gmail.com>
2023-08-04 19:58:10 -0400

    have an error, almost done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,23 +11,112 @@ export class AppComponent {
   calValue: number=0;
   functionT: any = 'Nofunction';
 
-  calNumber:string='noValue';
+  calNumber:string='noValue';//variable to check if there are any numbers in the screen
+
+  firstNumber: number=0
+  secondNumber: number=0
 
   onClickValue (val:string, type:any){
     if(type=='number'){
       this.onNumberClick(val);
     }
+    else if(type=='function'){
+      this.onFunctionClick(val);
+    }
   }
-
+//adds numbers to screen
   onNumberClick(val:string){
     if(this.calNumber != 'noValue'){
-      this.calNumber = this.calNumber +val;
+      this.calNumber = this.calNumber +val;//concatinating the numbers clicked
     }
     else{
       this.calNumber =val;
     }
     this.calValue = parseFloat(this.calNumber);
 
+  }
+
+  onFunctionClick(val:string){
+    //call clear sign method
+    if(val='C'){
+      this.clearSign();
+    }
+
+
+    else if(this.functionT=='Nofunction'){
+      this.firstNumber = this.calValue;
+      this.calValue=0;
+      this.calNumber='noValue';
+      this.functionT=val;
+
+    }
+    else if(this.functionT != 'Nofunction'){
+      this.secondNumber=this.calValue;
+      //calculation
+      this.valueCalc(val);
+
+    }
+  }
+  valueCalc(val:string){
+    if(this.functionT=='+'){
+      const Sum= this.firstNumber + this.secondNumber
+      this.totalAssignValues(Sum, val)
+      if(val== '='){
+        this.onEqualSign()
+      }
+    }
+    if(this.functionT=='-'){
+      const Sum= this.firstNumber - this.secondNumber
+      this.totalAssignValues(Sum, val)
+      if(val== '='){
+        this.onEqualSign()
+      }
+    }
+    if(this.functionT=='x'){
+      const Sum= this.firstNumber * this.secondNumber
+      this.totalAssignValues(Sum, val)
+      if(val== '='){
+        this.onEqualSign()
+      }
+    }
+    if(this.functionT=='/'){
+      const Sum= this.firstNumber/this.secondNumber
+      this.totalAssignValues(Sum, val)
+      if(val== '='){
+        this.onEqualSign()
+      }
+    }
+    if(this.functionT=='%'){
+      const Sum= this.firstNumber%this.secondNumber
+      this.totalAssignValues(Sum, val)
+      if(val== '='){
+        this.onEqualSign()
+      }
+    }
+  }
+
+  totalAssignValues(Sum: number, val:string){
+    this.calValue=Sum;
+    this.firstNumber=Sum;
+    this.secondNumber=0;
+    this.calNumber='noValue';
+    this.functionT=val;
+    
+  }
+
+  onEqualSign(){
+    this.firstNumber=0
+    this.secondNumber=0
+    this.functionT='Nofunction'
+    this.calNumber='noValue'
+  }
+
+  clearSign(){
+    this.firstNumber=0
+    this.secondNumber=0
+    this.calValue=0
+    this.functionT='Nofunction'
+    this.calNumber='noValue'
   }
 }
 
